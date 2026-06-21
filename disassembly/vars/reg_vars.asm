561e076525bca38c379d267a6cae1fb6f9cedd56
gocha <gochaism@gmail.com>
2026-06-21 10:58:54 +0900

    reg_vars.asm: Add PPU registers

--- disassembly/vars/reg_vars.asm
+++ disassembly/vars/reg_vars.asm
@@ -1,9 +1,79 @@
+; PPU Picture Processing Unit (Write-Only)
 !reg_inidisp = $2100
+!reg_obsel = $2101
+!reg_oamaddl = $2102
+!reg_oamadd = $2103
+!reg_oamdata = $2104
+!reg_bgmode = $2105
+!reg_mosaic = $2106
+!reg_bg1sc = $2107
+!reg_bg2sc = $2108
+!reg_bg3sc = $2109
+!reg_bg4sc = $210A
+!reg_bg12nba = $210B
+!reg_bg34nba = $210C
+!reg_bg1hofs = $210D
+!reg_bg1vofs = $210E
+!reg_bg2hofs = $210F
+!reg_bg2vofs = $2110
+!reg_bg3hofs = $2111
+!reg_bg3vofs = $2112
+!reg_bg4hofs = $2113
+!reg_bg4vofs = $2114
+!reg_vmain = $2115
+!reg_vmaddl = $2116
+!reg_vmaddh = $2117
+!reg_vmdatal = $2118
+!reg_vmdata = $2119
+!reg_m7sel = $211A
+!reg_m7a = $211B
+!reg_m7b = $211C
+!reg_m7c = $211D
+!reg_m7d = $211E
+!reg_m7x = $211F
+!reg_m7y = $2120
+!reg_cgadd = $2121
+!reg_cgdata = $2122
+!reg_w12sel = $2123
+!reg_w34sel = $2124
+!reg_wobjsel = $2125
+!reg_wh0 = $2126
+!reg_wh1 = $2127
+!reg_wh2 = $2128
+!reg_wh3 = $2129
+!reg_wbglog = $212A
+!reg_wobjlog = $212B
+!reg_tm = $212C
+!reg_ts = $212D
+!reg_tmw = $212E
+!reg_tsw = $212F
+!reg_cgwsel = $2130
+!reg_cgadsub = $2131
+!reg_coldata = $2132
+!reg_setini = $2133
 
+; PPU Picture Processing Unit (Read-Only)
+!reg_mpyl = $2134
+!reg_mpym = $2135
+!reg_mpyh = $2136
+!reg_slhv = $2137
+!reg_rdoam = $2138
+!reg_rdvraml = $2139
+!reg_rdvram = $213A
+!reg_rdcgram = $213B
+!reg_ophct = $213C
+!reg_opvct = $213D
+!reg_stat77 = $213E
+!reg_stat78 = $213F
+
+; APU Audio Processing Unit (R/W)
 !reg_apuio0 = $2140
 !reg_apuio1 = $2141
 !reg_apuio2 = $2142
 !reg_apuio3 = $2143
 
+; CPU On-Chip I/O Ports (Write-Only)
 !reg_nmitimen = $4200
+
+; CPU On-Chip I/O Ports (Read-Only)
 !reg_rdnmi = $4210
